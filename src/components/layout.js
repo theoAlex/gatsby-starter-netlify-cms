d0ba03fca85873c670fd103340e56ca310954eab
Alex Theobald <34870145+theoAlex@users.noreply.github.com>
2018-10-17 11:06:05 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,13 +7,16 @@ import '../layouts/all.sass'
     
 
 const Layout = ({ children }) => (
-  <img src="https://images.emojiterra.com/twitter/v11/512px/1f914.png" />
-  <div style={{ display: 'none' }} className="wrapper">
-    <Helmet title="Parlament Observer" />
-    <Navbar />
-    <div style={{ height: 'auto' }}>{children}</div>
-    <FooterComponent />
+  <div>
+    <img src="https://images.emojiterra.com/twitter/v11/512px/1f914.png" />
+    <div style={{ display: 'none' }} className="wrapper">
+        <Helmet title="Parlament Observer" />
+        <Navbar />
+        <div style={{ height: 'auto' }}>{children}</div>
+        <FooterComponent />
+    </div>
   </div>
+
 )
 
 export default Layout
