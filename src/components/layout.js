53cce8c8d922ddd5a6d5b1da6a5def704765a442
Alex Theobald <34870145+theoAlex@users.noreply.github.com>
2018-11-16 14:02:33 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import '../layouts/all.sass'
 const Layout = ({ children }) => (
   <div>
     <img src="https://images.emojiterra.com/twitter/v11/512px/1f914.png" />
-    <div className="wrapper">
+    <div style={{display: 'none'}} className="wrapper">
         <Helmet title="Parlament Observer" />
         <Navbar />
         <div style={{ height: 'auto' }}>{children}</div>
